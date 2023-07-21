6e93a7cd5a5099c9110e8267238b65d643b8bd8f
Jaromir007 <117089499+Jaromir007@users.noreply.github.com>
2023-07-21 18:38:48 +0200

    init

--- main.py
+++ main.py
@@ -471,30 +471,3 @@ robot.settings(Config.DRIVE_SPEED, Config.DRIVE_ACCELERATION, Config.TURN_RATE, 
 
 # explore the maze forever
 robot.explore()
-
-
-# MIT License for LEGO Mindstorms Maze Exploring Project
-# Author: Jaromir Obitko, Stepan Briza
-# Project Created at: Robocamp, directed by FEL (Faculty of Electrical Engineering)
-
-# MIT License
-# ------------
-# Copyright (c) Jaromir Obitko, Stepan Briza 2023
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
